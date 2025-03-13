8588e4f235f86f763a803e4868cfd9b75fbc68c4
Courdevelops <courdevelops@gmail.com>
2025-03-13 15:13:32 -0400

    try this in a new branch

--- js/database/localstorage.js
+++ js/database/localstorage.js
@@ -39,6 +39,9 @@ function registerUser(username, accountType, pin) {
         // Find user by name
         const user = users.find( u => u.username === username);
 
+        // Test print finding the user name
+        console.log(`The user found is: ${user}`);
+
         if(!user) {
             return { success: false, message: 'User not found'};
         }
@@ -65,8 +68,12 @@ function registerUser(username, accountType, pin) {
             }
         };
     }
-
 }
+
+// Query the same submit button from the confirmation page
+const submitButton = document.getElementById('confirmation_advance');
+
+console.log(`The Submit Button test: ${submitButton}`);
 
 
 // Submit the Name to Registration
@@ -85,10 +92,12 @@ function submitRegistration() {
     // Conditionals for the account type
     if (accountType === 'Manager') {
         // Redirect to the manager page
-        window.location.href = '../../html/manager/manager.html';
+        // window.location.href = '../../html/manager/manager.html';
+        window.location.href = '../../html/dashboards/manager.html';
     } else {
         // Redirect to the customer page
-        window.location.href = '../../html/calendar/calendar.html';
+        // window.location.href = '../../html/calendar/calendar.html';
+        window.location.href = '../../html/dashboards/employee.html';
     }
     
     return registerUser(username, accountType, pin);
@@ -96,18 +105,16 @@ function submitRegistration() {
 
 
 // Call the submitRegistration function upon submit button click
-// Query the submit button from the confirmation page
-const submitButton = document.getElementById('confirmation_advance');
 
 // Add an event listener to the submit button
 submitButton.addEventListener('click', (e) => {
+    // Test Print
+    console.log("Registration Submitted");
     // Prevent the default action of the submit button
     e.preventDefault();
 
     // Register the User
-    // registerUser();
-
-    // Call the submitRegistration function
+    // by calling the submitRegistration function
     submitRegistration();
 });
 
