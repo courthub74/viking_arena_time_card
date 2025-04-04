59edf2673c07d72337f2cd88baa92be96edff810
Courdevelops <courdevelops@gmail.com>
2025-04-04 16:22:43 -0400

    input so it should work with speech now

--- js/data/names.js
+++ js/data/names.js
@@ -15,32 +15,44 @@ const firstname = document.getElementById('firstname');
 // Query the last name
 const lastname = document.getElementById('lastname');
 
-// Function to check if the inputs are filled and enable the submit button regardless of the event type
-function checkInputs() {
+// Add event listener to the last name input to check if the first and last names are filled
+// Then enable submit button (by typing, pasting or speech)
+lastname.addEventListener('input', (e) => {
+    e.preventDefault();
     if (firstname.value && lastname.value) {
+        console.log("Both inputs are filled");
         submit_names.disabled = false;
     } else {
         submit_names.disabled = true;
     }
-}
-
-// Enable submit button when both inputs are filled
-
-// First Name
-firstname.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    checkInputs();
 });
 
-// Last Name
-lastname.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    checkInputs();
-});
+// // Function to check if the inputs are filled and enable the submit button regardless of the event type
+// function checkInputs() {
+//     if (firstname.value && lastname.value) {
+//         submit_names.disabled = false;
+//     } else {
+//         submit_names.disabled = true;
+//     }
+// }
 
-// Listen for the event input (captures all input methods including speech)
-firstname.addEventListener('input', checkInputs);
-lastname.addEventListener('input', checkInputs);
+// // Enable submit button when both inputs are filled by typing 
+
+// // First Name
+// firstname.addEventListener('keyup', (e) => {
+//     e.preventDefault();
+//     checkInputs();
+// });
+
+// // Last Name
+// lastname.addEventListener('keyup', (e) => {
+//     e.preventDefault();
+//     checkInputs();
+// });
+
+// // Listen for the event input (captures all input methods including speech)
+// firstname.addEventListener('input', checkInputs);
+// lastname.addEventListener('input', checkInputs);
 
 // Add event listener to submit button to register the user
 names_form.addEventListener('submit', (e) => {
