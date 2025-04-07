9f6b1626ad352439c549d6aa4a11fb834188ed75
Courdevelops <courdevelops@gmail.com>
2025-04-07 16:58:36 -0400

    new sidebars

--- js/sidebars.js
+++ js/sidebars.js
@@ -1,3 +1,17 @@
+// Test print the page
+console.log("Sidebars JS file loaded successfully.");
+
+// Get logged in user from session storage
+// Get Session Storage
+const logged_name = sessionStorage.getItem('username') ? decodeURIComponent(sessionStorage.getItem('username')) : 'Employee Name Here';
+const logged_acct_type = sessionStorage.getItem('acct_type') ? decodeURIComponent(sessionStorage.getItem('acct_type')) : 'N/A';
+
+// Query the name header
+const name_header = document.querySelector("#employee_name");
+// Query the slide in profile name
+const slide_in_profile_name = document.getElementById('profile_calendar_name');
+// Query the slide in account type
+const slide_in_account_type = document.getElementById('profile_calendar_account');
 
 
 
