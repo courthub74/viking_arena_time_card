d4b4fff8d0c15a13671c9c17e0254dd53b492195
Courdevelops <courdevelops@gmail.com>
2025-07-02 15:40:31 -0400

    ready to set view schedule calendar

--- js/dashboard/manager_buttons.js
+++ js/dashboard/manager_buttons.js
@@ -15,8 +15,13 @@ const manager_schedule = document.querySelector("#weeks_schedule");
 //GET MESSAGES
 const employee_calendar = document.querySelector("#employee_calendar");
 
+// SEE WEEK SCHEDULE
+const weekly_calendar = document.querySelector("#view_week_schedule");
+
 // Now we will add the event listeners to the buttons
 // MANAGER //
+
+// Get Employee Hours
 manager_hours.addEventListener("click", function(e) {
     e.preventDefault(); // Prevent the default action of the button
     manager_hours.classList.add("clicked");
@@ -29,6 +34,7 @@ manager_hours.addEventListener("click", function(e) {
     }, 1000);
 });
 
+// Get Weekly Schedule
 manager_schedule.addEventListener("click", function(e) {
     e.preventDefault(); // Prevent the default action of the button
     manager_schedule.classList.add("clicked");
@@ -40,6 +46,7 @@ manager_schedule.addEventListener("click", function(e) {
     }, 1000);
 });
 
+// Get Monthly Schedule
 employee_calendar.addEventListener("click", function(e) {
     e.preventDefault(); // Prevent the default action of the button
     employee_calendar.classList.add("clicked");
@@ -48,6 +55,18 @@ employee_calendar.addEventListener("click", function(e) {
     }, 1000);
     setTimeout(() => {
         window.location.href = "../../html/manager/manager_calendar.html"
+    }, 1000);
+});
+
+// Look at Weekly Calendar
+weekly_calendar.addEventListener("click", function(e) {
+    e.preventDefault(); // Prevent the default action of the button
+    weekly_calendar.classList.add("clicked");
+    setTimeout(() => {
+        weekly_calendar.classList.remove("clicked");
+    }, 1000);
+    setTimeout(() => {
+        window.location.href = "../../html/calendar/week_schedule.html";
     }, 1000);
 });
 
